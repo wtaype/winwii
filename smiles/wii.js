bf6e929600474736bc0446a486786fe7e0a37fa1
wtaype <wilder.taype16@gmail.com>
2026-03-29 19:29:32 -0500

    Actualizacion Principal v16.10.10

--- smiles/wii.js
+++ smiles/wii.js
@@ -5,15 +5,15 @@ export let lanzamiento = 2026;
 export let ipdev = import.meta.env.VITE_DEV;
 export let autor = '@wilder.taype';
 export let link = 'https://wtaype.github.io/';
-export let version = 'v15';
+export let version = 'v16';
 
 /** Actualizar main luego esto, pero si es mucho, solo esto. (1)
-git tag v15 -m "Version v15" ; git push origin v15 
+git tag v16 -m "Version v16" ; git push origin v16 
 
 //  ACTUALIZACIÓN PRINCIPAL ONE DEV [START] (2)
-git add . ; git commit -m "Actualizacion Principal v15.10.10" ; git push origin main
+git add . ; git commit -m "Actualizacion Principal v16.10.10" ; git push origin main
 
 // En caso de emergencia, para actualizar el Tag existente. (3)
-git tag -d v15 ; git tag v15 -m "Version v15 actualizada" ; git push origin v15 --force
+git tag -d v16 ; git tag v16 -m "Version v16 actualizada" ; git push origin v16 --force
  ACTUALIZACION TAG[END]  */ 
 
